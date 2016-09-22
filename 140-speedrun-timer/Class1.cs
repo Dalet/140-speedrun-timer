7901d0306ddbdefcdf396d1384d6bee98587ee60
Dalet <dmolet@student.42.fr>
2016-09-23 01:28:31 +0200

    Add project files.

---
+++ 140-speedrun-timer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _140_speedrun_timer
+{
+    public class Class1
+    {
+    }
+}
