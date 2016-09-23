f631f38dc509458e2dff1c8d76b076db0edf1156
Dalet <dmolet@student.42.fr>
2016-09-24 01:19:19 +0200

    round milliseconds

--- 140-speedrun-timer/Utils.cs
+++ 140-speedrun-timer/Utils.cs
@@ -8,11 +8,11 @@ namespace SpeedrunTimerMod
 		{
 			var seconds = totalSeconds % 60;
 			var minutes = (int)(totalSeconds / 60);
-			var miliseconds = Math.Truncate((totalSeconds - (int)totalSeconds) * 1000);
+			var milliseconds = Math.Round((totalSeconds - (int)totalSeconds) * 1000);
 
 			var minutesStr = minutes.ToString();
 			var secondsStr = ((int)seconds).ToString().PadLeft(2, '0');
-			var millisecondsStr = miliseconds.ToString().PadLeft(3, '0');
+			var millisecondsStr = milliseconds.ToString().PadLeft(3, '0');
 
 			return $"{minutes}:{secondsStr}.{millisecondsStr}";
 		}
