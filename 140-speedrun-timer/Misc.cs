4bd7a0b94b5c7e18cb6738c525a7688c83f293e2
Dalet <dmolet@student.42.fr>
2016-10-02 19:06:30 +0200

    beat debug info

--- 140-speedrun-timer/Misc.cs
+++ 140-speedrun-timer/Misc.cs
@@ -7,6 +7,26 @@ namespace SpeedrunTimerMod
 		float resetTimer;
 		float resetHits;
 
+		static int _beatIndex = -1;
+		public static int BeatIndex
+		{
+			get { return _beatIndex; }
+			private set
+			{
+				_beatIndex = value;
+				MakeDebugString();
+			}
+		}
+
+		public static string BeatDbgStr { get; private set; }
+
+		public void Start()
+		{
+			BeatIndex = -1;
+			Globals.beatMaster.onBeat += OnBeat;
+			Globals.beatMaster.onBeatReset += OnBeatReset;
+		}
+
 		public void Update()
 		{
 			if (Application.isLoadingLevel)
@@ -31,6 +51,28 @@ namespace SpeedrunTimerMod
 					Application.LoadLevel("Level_Menu");
 				}
 			}
+		}
+
+		void OnBeat(int index)
+		{
+			BeatIndex = index;
+		}
+
+		void OnBeatReset()
+		{
+			BeatIndex = -1;
+		}
+
+		static void MakeDebugString()
+		{
+			if (_beatIndex < 0)
+			{
+				BeatDbgStr = "-";
+				return;
+			}
+
+			var beat = _beatIndex + 1;
+			BeatDbgStr = $"{beat.ToString().PadLeft(2, '0')}/16";
 		}
 	}
 }
