c5a563c92c1781d7e020dba6bf9f2ca9645b9a6d
Dalet <dmolet@outlook.com>
2017-07-14 21:19:24 +0200

    bump version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.6.1.6")]
-[assembly: AssemblyFileVersion("0.6.1.6")]
+[assembly: AssemblyVersion("0.6.1.7")]
+[assembly: AssemblyFileVersion("0.6.1.7")]
+
