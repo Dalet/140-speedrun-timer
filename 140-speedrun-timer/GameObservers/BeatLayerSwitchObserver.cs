68b00e44556f907cfeafdbc18d580bd6dfd6086e
Dalet <dmolet@outlook.com>
2017-09-30 13:52:39 +0200

    Split on sphere expansion merge MenuSystemObserver with BeatLayerSwitchObserver

--- 140-speedrun-timer/GameObservers/BeatLayerSwitchObserver.cs
+++ 140-speedrun-timer/GameObservers/BeatLayerSwitchObserver.cs
@@ -6,22 +6,40 @@ namespace SpeedrunTimerMod.GameObservers
 	class BeatLayerSwitchObserver : MonoBehaviour
 	{
 		GameObject _levelsFolder;
+		MenuSystem _menuSystem;
 
 		void Awake()
 		{
 			_levelsFolder = GameObject.Find("Levels");
 			if (_levelsFolder == null)
 			{
+				Debug.Log($"{nameof(BeatLayerSwitchObserver)}: Couldn't find Levels object");
 				Destroy(this);
 				return;
 			}
+
+			var menuSystemObj = GameObject.Find("_MenuSystem");
+			if (menuSystemObj != null)
+				_menuSystem = menuSystemObj.GetComponent<MenuSystem>();
+			else
+				Debug.Log($"{nameof(BeatLayerSwitchObserver)}: Couldn't find _MenuSystem object");
 		}
 
 		void OnEnable()
 		{
 			var colorSpheres = _levelsFolder.GetComponentsInChildren<ColorSphere>();
 			foreach (var colorSphere in colorSpheres)
-				colorSphere.colorSphereOpened += OnKeyUsed;
+			{
+				if (_menuSystem != null && colorSphere == _menuSystem.colorSphere)
+				{
+					colorSphere.colorSphereOpened += OnMenuKeyUsed;
+					colorSphere.colorSphereExpanding += OnMenuColorSphereExpanding;
+				}
+				else
+				{
+					colorSphere.colorSphereExpanding += OnColorSphereExpanding;
+				}
+			}
 		}
 
 		void OnDisable()
@@ -31,13 +49,38 @@ namespace SpeedrunTimerMod.GameObservers
 
 			var colorSpheres = _levelsFolder.GetComponentsInChildren<ColorSphere>();
 			foreach (var colorSphere in colorSpheres)
-				colorSphere.colorSphereOpened -= OnKeyUsed;
+			{
+				if (_menuSystem != null && colorSphere == _menuSystem.colorSphere)
+				{
+					colorSphere.colorSphereOpened -= OnMenuKeyUsed;
+					colorSphere.colorSphereExpanding -= OnMenuColorSphereExpanding;
+				}
+				else
+				{
+					colorSphere.colorSphereExpanding -= OnColorSphereExpanding;
+				}
+			}
 		}
 
-		void OnKeyUsed()
+		void OnColorSphereExpanding()
 		{
-			Debug.Log("OnKeyUsed: " + DebugBeatListener.DebugStr);
-			SpeedrunTimer.Instance?.Split(150, -1);
+			Debug.Log("BeatLayerSwitch colorsphere expanding: " + DebugBeatListener.DebugStr);
+			SpeedrunTimer.Instance?.Split();
+		}
+
+		void OnMenuColorSphereExpanding()
+		{
+			Debug.Log("OnMenuColorSphereExpanding: " + DebugBeatListener.DebugStr);
+
+			// colorsphere expands 32 quarterbeats after opening, then starts the load after 0.66s
+			SpeedrunTimer.Instance?.Split();
+			SpeedrunTimer.Instance?.StartLoad(660);
+		}
+
+		void OnMenuKeyUsed()
+		{
+			Debug.Log("OnMenuKeyUsed: " + DebugBeatListener.DebugStr);
+			SpeedrunTimer.Instance?.Unfreeze();
 		}
 	}
 }
